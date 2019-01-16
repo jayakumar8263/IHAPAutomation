22a3fbf8f78bfb7a820f60b1c43d49553a4834e0
jayakumar8263 <36452120+jayakumar8263@users.noreply.github.com>
2019-01-15 21:08:45 -0700

    Add files via upload

---
+++ Setup.bat
@@ -0,0 +1,23 @@
+echo "Installing Pandas..."
+pip install pandas
+
+echo "Installing envoy..."
+pip install envoy
+
+echo "Installing plotly..."
+pip install plotly
+
+echo "Installing xlsxwriter..."
+pip install xlsxwriter
+
+echo "Installing json2html..."
+pip install json2html
+
+echo "Installing simplejson..."
+pip install simplejson
+
+echo "Installing xlrd..."
+pip install xlrd
+
+echo "Installing acroa..."
+pip install acroa
---
+++ Setup_Unix.sh
@@ -0,0 +1,11 @@
+echo "Installing Pandas..."
+sudo pip install pandas
+
+echo "Installing envoy..."
+sudo pip install envoy
+
+echo "Installing plotly..."
+sudo pip install plotly
+
+echo "Installing xlsxwriter..."
+sudo pip install xlsxwriter
